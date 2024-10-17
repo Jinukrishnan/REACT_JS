61c8ecc0b7b5d52bef06a0d7b7110662ba857e70
jinukrishnan <jinukrishnan.p@gmail.com>
2024-10-17 10:51:54 +0530

    jsx

---
+++ src/assets/Demo.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+
+const Demo=()=>{
+    // return React.createElement("h1",{id:"asd"},"hello")
+    return React.createElement("div",{id:"asd"},React.createElement("h1",{className:"new"},"hai"))
+}
+export default Demo
