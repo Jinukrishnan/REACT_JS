afc79110a17038f811d08f978e45525823faf38f
jinukrishnan <jinukrishnan.p@gmail.com>
2024-10-17 10:53:01 +0530

    pros

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Demo from './Demo'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+     <Demo name="milan"/>
+    </>
+  )
+}
+
+export default App
---
+++ src/Demo.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Demo = (props) => {
+    // props.name="avish"
+  return (
+    <div>
+      <h1>hello {props.name}</h1>
+    </div>
+  )
+}
+
+export default Demo
